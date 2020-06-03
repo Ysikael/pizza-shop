98272a325e9e333ddd03890168d3e5276ca16fbd
Krystel <Krystelmenot@gmail.com>
2020-06-03 17:57:53 +0200

    feat: 🎸 premiere version de pizza card

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
 
 import Header from "../Header";
 import { theme } from "./theme";
-import { CssBaseline } from "@material-ui/core";
-import { ThemeProvider } from "@material-ui/core/styles";
+import PizzaCard from "../PizzaCard";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header ShoppingCartCount={3} />
+      <PizzaCard name="Reine" />
     </ThemeProvider>
   );
 }
